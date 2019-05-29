4a873dc40e2bd00295ba9d12204f5946f928527f
Sandeep <sandeep_neela_2@progressive.com>
2019-05-29 15:07:43 +0000

    New Changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Front-End Web 200 With Angular';
+  title = 'Front-End Web 200 With Angular!!';
 }
