15f42af233e68df3be31ca6b42ad86216d703799
Sandeep <sandeep_neela_2@progressive.com>
2019-05-30 13:41:50 +0000

    enrty and list

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,8 @@ import { TodoListComponent } from './components/todo-list/todo-list.component';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
 import { TipCalculatorComponent } from './components/tip-calculator/tip-calculator.component';
 import { NavComponent } from './components/nav/nav.component';
+import { EntryComponent } from './components/todo-list/entry/entry.component';
+import { ListComponent } from './components/todo-list/list/list.component';
 
 @NgModule({
   declarations: [
@@ -14,7 +16,9 @@ import { NavComponent } from './components/nav/nav.component';
     TodoListComponent,
     DashboardComponent,
     TipCalculatorComponent,
-    NavComponent
+    NavComponent,
+    EntryComponent,
+    ListComponent
   ],
   imports: [
     BrowserModule,
