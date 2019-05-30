45379c85cf7b4cfe8035253636d96ed39bc8e76f
Sandeep <sandeep_neela_2@progressive.com>
2019-05-30 13:56:17 +0000

    todo-list refractor

--- src/app/components/todo-list/entry/entry.component.ts
+++ src/app/components/todo-list/entry/entry.component.ts
@@ -12,4 +12,7 @@ export class EntryComponent implements OnInit {
   ngOnInit() {
   }
 
+  add(what: HTMLInputElement) {
+
+  }
 }
--- src/app/components/todo-list/list/list.component.ts
+++ src/app/components/todo-list/list/list.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { TodoListItem } from '../models';
 
 @Component({
   selector: 'app-list',
@@ -7,9 +8,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ListComponent implements OnInit {
 
+  @Input() list: TodoListItem[];
+
   constructor() { }
 
   ngOnInit() {
   }
 
+  markComplete(item: TodoListItem) {
+
+  }
+
+  clearCompleted() {
+
+  }
 }
