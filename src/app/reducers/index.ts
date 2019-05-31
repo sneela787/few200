09e0c63634574c3aa2747b8b7bd60dea8feea3cc
Sandeep <sandeep_neela_2@progressive.com>
2019-05-31 11:22:14 -0400

    Index

--- src/app/reducers/index.ts
+++ src/app/reducers/index.ts
@@ -14,4 +14,4 @@ export const selectCounter = (state: State) => state.counter;
 export const selectCurrentCount = (state: State) => state.counter.current;
 export const selectCountingBy = (state: State) => state.counter.by;
 export const selectAtTheStart = createSelector(selectCurrentCount, selectCountingBy, (c, b) => c - b < 0);
-export const atTheZero = createSelector(selectCurrentCount, (c) => c === 0);
+export const atTheZero = createSelector(selectCurrentCount, c => c === 0);
